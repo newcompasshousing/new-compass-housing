420baa2641c05828c943af13bac873642ed16da5
Davon Walker <davonwalker@MacBookPro.lan>
2026-04-27 17:40:22 -0700

    simplify resizable component

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -1,47 +1,39 @@
 "use client"
 
 import * as React from "react"
-import {
-  Panel,
-  PanelGroup,
-  PanelResizeHandle,
-} from "react-resizable-panels"
-
 import { cn } from "@/lib/utils"
 
-const ResizablePanelGroup = ({
+function ResizablePanelGroup({
+  className,
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn("flex h-full w-full", className)} {...props}>
+      {children}
+    </div>
+  )
+}
+
+function ResizablePanel({
+  className,
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn("flex-1", className)} {...props}>
+      {children}
+    </div>
+  )
+}
+
+function ResizableHandle({
   className,
   ...props
-}: React.ComponentProps<typeof PanelGroup>) => (
-  <PanelGroup
-    className={cn(
-      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-      className
-    )}
-    {...props}
-  />
-)
-
-const ResizablePanel = Panel
-
-const ResizableHandle = ({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & {
+}: React.HTMLAttributes<HTMLDivElement> & {
   withHandle?: boolean
-}) => (
-  <PanelResizeHandle
-    className={cn(
-      "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2",
-      className
-    )}
-    {...props}
-  >
-    {withHandle && (
-      <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-sm border" />
-    )}
-  </PanelResizeHandle>
-)
+}) {
+  return <div className={cn("w-px bg-border", className)} {...props} />
+}
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
